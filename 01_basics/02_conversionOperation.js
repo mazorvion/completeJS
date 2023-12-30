69f5e95accc71fb661cae3611d6fee21afd8457d
mzrv <vikask0101@outlook.com>
2023-12-30 14:51:47 +0530

    comparison

--- 01_basics/02_conversionOperation.js
+++ 01_basics/02_conversionOperation.js
@@ -48,3 +48,81 @@ console.log("string conversion")
 let someNumber = 33
 let stringNumber = String(33)
 console.log(someNumber) // 33 and type is string
+
+
+// ********************** Operations ************************
+console.log('operations:')
+
+let value = 3;
+let negValue = -value
+// console.log(negValue) // -3
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2) //remainder
+
+// let str1 = "Hello"
+// let str2 = "Vikas"
+// let str3 = str1 + str2
+// console.log(str3) // HelloVikas
+
+// console.log("1" + 2) //12 
+// console.log("1" + 2 + 5) //12 
+// console.log(1 + 2 + "5") //12 
+
+// console.log(+true) //1
+// console.log(true+2) //3
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// console.table([num1, num2, num3]) // 4, 4, 4
+
+// let gameCounter = 100
+// gameCounter++; // 101
+// ++gameCounter; // 101
+// console.log(gameCounter) // 101
+
+
+// let x = 3;
+// const y = x++; // it adds + 1 to x after the assignment to y
+// // so y will have it's previous value which is 3 and
+// // x will have 4
+// console.log(x)
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a; // it adds + 1 before the assignment to b
+// // that is why a is 4 as well as b is 4 here
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+
+// ******************COMPARISONS******************
+
+// console.log(2>1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0) // false
+// console.log(null == 0) // false
+// console.log(null >= 0) // true
+
+// console.log(undefined > 0) //false
+// console.log(undefined == 0) // false
+// console.log(undefined >= 0) // false
+
+console.log("2" == 2) // true because it'll convert
+console.log("2" === 2) // false because this is strict comparison
