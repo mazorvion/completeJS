0d9985d8a9ff205b93a4604db4c9599c854aa346
mzrv <vikask0101@outlook.com>
2023-12-30 13:15:14 +0530

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all JS code as newer js
+            // this simply means that engine will
+           // treat all codes in this file as newer js
+
+// alert("hello")  // cannot be used in nodejs, but in browser
+
+// console.log(3 + 3); console.log("vikas") // bad readability
+
+let name = "vikas"
+let age = 22
+let isLoggedIn = false
+let temp = null // type of null is object
+let temp2 = temp * 5656  // if i do arthematic with null it'll be considered as zero 
+// number => 2 to the power of 53 
+// bigint => for high numbers
+// string => "" , ''
+// boolean => true , false
+// null => standalone value
+// undefined => type of undefined is undefined
+// symbol => used for uniqueness
+
+
+
+//object
+
+// console.log(typeof null) // object
+// console.log(typeof undefined) // undefined
+
+
