1d12e1ec71325fb433b293dc59a5dc9cc8fe0c70
mzrv <vikask0101@outlook.com>
2024-01-05 19:14:46 +0530

    destructuring of objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -44,10 +44,58 @@ const users = [
     }
 ]
 
-console.log(users[1].email)
-console.log(tinderUser)
-console.log(Object.keys(tinderUser)) // this will give an array of all keys
-console.log(Object.values(tinderUser)) // this will give an array of all values
-console.log(Object.entries(tinderUser))// this will return each key value pair in array 
+// console.log(users[1].email)
+// console.log(tinderUser)
+// console.log(Object.keys(tinderUser)) // this will give an array of all keys
+// console.log(Object.values(tinderUser)) // this will give an array of all values
+// console.log(Object.entries(tinderUser))// this will return each key value pair in array 
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn')) // this check if tinderuser object has iLoggedIn Property
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')) // this check if tinderuser object has iLoggedIn Property
+
+
+
+
+
+
+// ******************* DESTRUCTURING OBJECTS ************************//
+
+const course = {
+    coursename: "js mastery",
+    price: "999",
+    courseInstructor: "satya nadella",
+}
+
+const {courseInstructor: instructor} = course
+// courseInstructor is the property we are getting from the course object and then we can access it either with the name of property or we can also give it a name of our own after semicolon like i've given it above and i'll be able to access that property with that new name whatever is written after the instructor
+
+console.log(instructor) // OUTPUT: satya nadella
+// console.log(courseInstructor) // this will throw an error because name has been changed to instructor
+
+const {price} = course
+console.log(price)// OUTPUT: 999
+
+
+// const myObject = {
+//     "name": "John",   // String key
+//     42: "Answer",      // Number key
+//     "isStudent": true  // String key
+//   };
+  
+//   console.log(myObject.name);    // Outputs: John
+//   console.log(myObject[42]);      // Outputs: Answer
+//   console.log(myObject.isStudent); // Outputs: true
+
+
+//   const myObject2 = {
+//     true: "It's true!",   // Boolean true is converted to string "true"
+//     false: "It's false!"  // Boolean false is converted to string "false"
+//   };
+  
+//   console.log(myObject2.true);   // Outputs: It's true!
+//   console.log(myObject2.false);  // Outputs: It's false!
+
+  
+
+
+
+
