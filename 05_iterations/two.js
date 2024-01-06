8d0fa5fbf194fb33fc7efe7b3ab8aaf51e64ba7d
mzrv <vikask0101@outlook.com>
2024-01-06 15:57:47 +0530

    do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,40 @@
+// let i = 1
+
+// while (i) {
+//     if (i > 12){
+//         break
+//     }
+//     console.log('true')
+
+//     i++
+// }
+
+
+// let index = 2
+// while (index <= 10) {
+//     console.log(`value of index i ${index}`)
+//     index += 2
+// }
+
+
+// let heroes = ["flash", "batman", "superman"]
+// let arr = 0
+// while (arr < heroes.length) {
+//     console.log(heroes[arr])
+//     arr++
+// }
+
+// let score = 2
+
+// do {
+//     console.log(`Score is ${score}`)
+//     score += 2
+// } while (score <= 20);
+
+// what if the score is already greater than the condition
+let score2 = 21
+do {
+    console.log(`Score is ${score2}`)
+    score2 += 2
+} while (score2 <= 20);
+// OUTPUT: Score is 21 because it first runs the code and then it checks the condition, do while loop always checks the condition in the end
